7e6c970d0b7ce8fbb617098410b3bc6ffd54d178
Prem Yadav <prem05082002@gmail.com>
2025-05-29 22:42:03 +0530

    toast error fixed

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -1,25 +1,31 @@
 import { useToast } from "@/hooks/use-toast"
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <div className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
+    <ToastProvider>
       {toasts.map(({ id, title, description, action, ...props }) => (
-        <div
-          key={id}
-          className="bg-background text-foreground data-[swipe=move]:transition-none group relative pointer-events-auto flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full"
-          {...props}
-        >
+        <Toast key={id} {...props}>
           <div className="grid gap-1">
-            {title && <div className="text-sm font-semibold">{title}</div>}
+            {title && <ToastTitle>{title}</ToastTitle>}
             {description && (
-              <div className="text-sm opacity-90">{description}</div>
+              <ToastDescription>{description}</ToastDescription>
             )}
           </div>
           {action}
-        </div>
+          <ToastClose />
+        </Toast>
       ))}
-    </div>
+      <ToastViewport />
+    </ToastProvider>
   )
 } 
