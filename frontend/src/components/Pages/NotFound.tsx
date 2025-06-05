d09d715cb68aa9d9f71ab0ab3136b468ea5e3f5e
Waseem Ahemed <78431077+waseemahemed@users.noreply.github.com>
2025-06-06 00:52:39 +0530

    Dynamic Routes In NotFound Page

--- frontend/src/components/Pages/NotFound.tsx
+++ frontend/src/components/Pages/NotFound.tsx
@@ -1,24 +1,50 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const NotFound = () => {
   const location = useLocation();
+
+    const navigate = useNavigate();
+
+  const [page, setPage] = useState("Dashboard");
+
+  const getrole = localStorage.getItem('role');
 
   useEffect(() => {
     console.error(
       "404 Error: User attempted to access non-existent route:",
       location.pathname
     );
+
+    if (getrole === null) {
+      setPage("Home")
+    }else{
+      setPage("Dashboard")
+    }
+
   }, [location.pathname]);
+
+  const goToDashboard = () =>{
+     
+    if (getrole === "student") {
+         navigate('/student');
+      }else if (getrole === "teacher"){
+        navigate('/teacher');
+      }else if (getrole === "admin"){
+        navigate('/admin');
+      }else {
+        navigate('/')
+      }
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-primary mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-6">The page you're looking for doesn't exist.</p>
-        <Button asChild>
-          <a href="/">Return to Dashboard</a>
+        <Button asChild onClick={goToDashboard}>
+          <a>Return to {page}</a>
         </Button>
       </div>
     </div>
