dd757794bbef2e32a54af8e0ddffeae16caf7bb5
varruunnn <varun.choudhary2512@gmail.com>
2025-07-04 22:29:36 +0530

    ok

--- backend/src/models/Assignment.js
+++ backend/src/models/Assignment.js
@@ -3,37 +3,37 @@ import mongoose from 'mongoose';
 const assignmentSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   subject: {
     type: String,
-    required: true
-  },
-  description: String,
-  dueDate: {
-    type: Date,
-    required: true
-  },
-  time: String,      
-  location: String,  
-  topics: [String],   
-  schoolId: {
-    type: Number,
-    required: true
+    required: true,
   },
   className: {
     type: String,
-    required: true
+    required: true,
+  },
+  topics:{
+    type:Array,
+  },
+  sec:{
+    type:String,
+  },
+  description: String,
+  fileUrl: String,
+  dueDate: Date,
+  schoolId: {
+    type: Number,
+    required: true,
   },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
-
 export default mongoose.model('Assignment', assignmentSchema);
