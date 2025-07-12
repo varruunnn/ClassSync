85544c760fa66f82a02313051d4adf962447a27d
varruunnn <varun.choudhary2512@gmail.com>
2025-07-12 23:04:06 +0530

    added rate limit login

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import { register, login ,logout,me , changePassword} from '../controllers/authController.js'; 
 import protect from '../middlewares/auth.js';
+import rateLimit from 'express-rate-limit';
 const router = express.Router();
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5, 
+  message: {
+    message: 'Too many login attempts, please try again after 15 minutes.'
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login',loginLimiter, login);
 router.post('/logout', logout);
 router.get("/me", protect, me);
 router.put("/change-password",protect,changePassword);
